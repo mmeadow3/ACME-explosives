7f7960062d950bba3e565b15c33a2d5f10a3bd96
Mike Meadows <mmeadow33@gmail.com>
2016-06-22 13:40:18 -0500

    added notes

--- loader.js
+++ loader.js
@@ -1,63 +1,79 @@
 var products = [];
 var types = [];
 var categories = [];
-//////separate each AJAX call for readablility///////////
-var getCategories = function() {
-    return new Promise((resolve, reject) => {
-        $.ajax({
-            url: "categories.json"
-        }).done(function(data) {
-            resolve(data.categories);
-        }).fail(function(xhr, status, error) {
-            reject(error);
-        });
-    })
-};
-///////////////Second type of AJAX request//////////////
-var getTypes = function(getCategories) {
-  return new Promise((resolve, reject) => {
-    $.ajax({
-      url: "types.json",
-      data: getCategories
-    }).done(function(data) {
-      resolve(data);
-    }).fail(function(xhr, status, error) {
-      reject(error);
-    });
-  })
-};
 
-/////////// This function does one thing, and returns a promise//////////
-var getProducts = function(getTypes) {
-  return new Promise((resolve, reject) => {
-    $.ajax({
-      url: "products.json",
-      data: getTypes
-    }).done(function(data) {
-      resolve(data);
-    }).fail(function(xhr, status, error) {
-      reject(error);
-    });
-  })
-};
-/////////////Now we use those Promises to describe the order of execution, 
- //////// and how data flows between each one.
+$(document).ready(function() {
 
-/////////////Note how the resolve callback function, itself, returns another 
-/////////// promise. This is how you can chain promises, and dictate the
-//////////// order of execution of multiple aynschronous operations.
-getCategories()
+    var firstXHR = function() {
+        return new Promise((resolve, reject) => {
+            $.ajax({
+                url: "categories.json"
+            }).done(function(data) {
+                resolve(data.categories); //////////is getting into the JSON file 
+            }).fail(function(xhr, status, error) {
+                reject(error);
+            });
+        })
+    };
+
+    var secondXHR = function(result_of_firstXHR) {
+        return new Promise((resolve, reject) => {
+            $.ajax({
+                url: "types.json",
+                data: result_of_firstXHR
+            }).done(function(data) {
+                resolve(data.types); //////////is getting into the JSON file 
+            }).fail(function(xhr, status, error) {
+                reject(error);
+            });
+        })
+    };
+
+    var thirdXHR = function(result_of_secondXHR) {
+        return new Promise((resolve, reject) => {
+            $.ajax({
+                url: "products.json",
+                data: result_of_secondXHR
+            }).done(function(data) {
+                resolve(data.products); //////////is getting into the JSON file 
+            }).fail(function(xhr, status, error) {
+                reject(error);
+            });
+        })
+    };
+
+
+firstXHR()
   .then(function(data1) {
-    return getTypes(data1);
-  })
+    categories = data1;
+    console.log("cat", data1);
+    return secondXHR(data1);
+})
   .then(function(data2) {
-    return getProducts(data2);
-  }).then(function(data3){
-    console.log(data3);  /////// Logs out an Object//////////
-  });
+    types = data2;
+    console.log("type", data2);
+    return thirdXHR(data2);
+})
+  .then(function(data3) {
+    products = data3;
+    console.log("products", data3);
+})
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+})
 
 
 
